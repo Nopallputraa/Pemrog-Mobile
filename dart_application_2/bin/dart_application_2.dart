10e749508548530c08150bc66d15883da32dd071
Naufal Putra Azizi Rahman <nopallpa@gmail.com>
2024-09-12 13:26:08 +0700

    first commit

---
+++ dart_application_2/bin/dart_application_2.dart
@@ -0,0 +1,11 @@
+import 'package:dart_application_2/dart_application_2.dart' as dart_application_2;
+
+//void main(List<String> arguments) {
+  //print('Hello world: ${dart_application_2.calculate()}!');
+//}
+
+void main() {
+  for (int i = 20; i > 10; i--) {
+    print('Nama saya Fulan, Sekarang berumur ${i - 2}');
+  }
+}
