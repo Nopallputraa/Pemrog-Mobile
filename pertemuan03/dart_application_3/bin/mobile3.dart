2690753986bb0e38f16d632995fc0a4051031fa9
Naufal Putra Azizi Rahman <nopallpa@gmail.com>
2024-09-19 10:51:25 +0700

    laporan js3 mobile

--- pertemuan03/dart_application_3/bin/mobile3.dart
+++ pertemuan03/dart_application_3/bin/mobile3.dart
@@ -2,9 +2,9 @@ void main() {
   int index = 0;
   for (index; index < 27; index++) {
     print(index);
-    if (index == 21)
+    if (index == 21) {
       break;
-    else if (index > 1 || index < 7) continue;
+    } else if (index > 1 || index < 7) continue;
     print(index);
   }
 }
